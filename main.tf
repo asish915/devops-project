06aae5f899b3948e96fab9e0f8c79c0ea84b09a3
AsishSarangi <asishsarangi2005@gmail.com>
2025-07-17 17:43:01 +0530

    changes made

--- main.tf
+++ main.tf
@@ -4,10 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.3"
     }
   }
 }
+
 
 provider "aws" {
   region = ap-south-1  #Provided here
